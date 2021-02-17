069b7a7dde7c65524fe6c264df96308dbd6ec597
Abiola Baruwa <abiola.baruwa.business@gmail.com>
2021-02-17 16:21:26 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,11 +1,15 @@
 function basicTeenager(age) {
+<<<<<<< HEAD
   if (ageIs13-19) {
     "You are a teenager!"()
 }
+=======
+  if ('age' = 13-19)
+  greeting = "You are a teenager!";
+  }
+>>>>>>> b0e9683fd9da2bdc97f30f3780c6683a833a0330
 
 function teenager(age) {
-
-}
 
 function ageChecker(age) {
 
